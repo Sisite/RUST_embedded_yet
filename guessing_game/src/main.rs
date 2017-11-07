c6878af8c04850284954693ca65e58e0ade4fdd8
Robin Jonsson <jonrob-3@student.ltu.se>
2017-11-07 15:27:00 +0100

    Finished assigment 2a

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,22 +2,19 @@ extern crate rand;
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+//use std::result;
+//use std::string;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1,101);
     let mut cnt = 0;
     loop {
         println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess)
-          .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(e) => {println!("ERROR: Non integer input : {}", e);  continue;}
+        let guess : u32 = match line_input() {
+            Ok(num) => {num}
+            Err(e) => {println!("{}", e); continue;}
         };
+
 
         println!("You guessed: {}", guess);
         cnt +=1;
@@ -31,5 +28,17 @@ fn main() {
             }
         }
     }
+
+}
+fn line_input() -> Result<u32,String>  {
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+      .expect("Failed to read line");
+
+        match guess.trim().parse() {
+        Ok(num) => return Ok(num),
+        Err(e) => return Err(String::from(format!("in parsing u32, {}", e))), //println!("ERROR: Non integer input : {}", e);  continue;}
+    };
 
 }
