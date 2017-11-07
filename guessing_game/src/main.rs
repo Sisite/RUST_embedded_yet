1873171025e6bef63ff442bdfbd2c6a54f6d8f07
Robin Jonsson <jonrob-3@student.ltu.se>
2017-11-07 16:01:59 +0100

    Finished assignment 2b

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1,101);
     let mut cnt = 0;
+    let mut vector_pair = vec![];
     loop {
         println!("Please input your guess.");
         let guess : u32 = match line_input() {
@@ -18,18 +19,28 @@ fn main() {
 
         println!("You guessed: {}", guess);
         cnt +=1;
+        vector_pair.push((cnt, guess.to_string()));
         println!("Number of tries: {}", cnt);
         match guess.cmp (&secret_number) {
                 Ordering::Less => println!("Too small!"),
                 Ordering::Greater => println!("Too big!"),
                 Ordering::Equal => {
                     println!("You win!");
+                    victory_happend(vector_pair);
                     break;
             }
         }
     }
 
 }
+fn victory_happend(vector_pair :Vec<(u32, String)>) ->(){
+    for x in vector_pair.iter() {
+        println!("Count: {}, Guess: {}", x.0, x.1);
+        //println!("Guess: {}", x.1);
+    }
+
+}
+
 fn line_input() -> Result<u32,String>  {
     let mut guess = String::new();
 
